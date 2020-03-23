1fed66e932ed3f31760b845526c4eea2410eb224
Seungho320 <splovetw@gmail.com>
2020-03-23 13:39:35 +0900

    fix signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
 
   private
@@ -9,12 +9,15 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  # def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       # username == 'admin' && password == '2222'
     end
   end
+  
 end
