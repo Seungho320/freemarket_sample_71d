38bcf9834fec3468183112b39924800c7f226319
jun1t <junichi.hobby@gmail.com>
2020-03-24 14:54:35 +0900

    brands active_hash

--- app/models/brands.rb
+++ app/models/brands.rb
@@ -3,7 +3,7 @@ class Brands < ActiveHash::Base
   self.data = [
       { id: 1, name: 'シャネル' },
       { id: 2, name: 'ナイキ' },
-      { id: 3, name: 'ルイ　ヴィトン' },
+      { id: 3, name: 'ルイ ヴィトン' },
       { id: 4, name: 'シュプリーム' },
       { id: 5, name: 'アディダス' },
       { id: 6, name: '不明' },
