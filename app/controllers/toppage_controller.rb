6becb3f0cb9145e4769b7807a218cd585c96513b
Seungho320 <splovetw@gmail.com>
2020-03-24 21:02:42 +0900

    updsate プルダウン用　controller

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,6 +1,16 @@
 class ToppageController < ApplicationController
 
   def index
+    @items = Item.limit(10).order('created_at DESC')
+    @category = Categories.all
+
+    @main_category = Categories.where(sub: '0')
+    @categories = Categories.where(sub: params[:sub], sub_sub: params[:sub_sub])
+    # @sub_sub_category = Category.where(sub: params[:sub], sub_sub: params[:sub_sub] )
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
 end
