530882258fb8521a89a0865b2c1dde4a10c3861c
ryo6439 <61182531+ryo6439@users.noreply.github.com>
2020-03-24 19:05:55 +0900

    s3条件分岐

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'deploy-mercari'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/deploy-mercari'
+  if Rails.env.development? || Rails.env.test? 
+    config.storage = :file
+  elsif Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'deploy-mercari'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/deploy-mercari'
+  end
 end
