8bb71021413d2551b044c543b28d55a7b4e847c8
hoshinoshinsuke <xingyechenjie0608@icloud.com>
2020-03-24 21:34:31 +0900

    front-sell-index

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -1,5 +1,10 @@
 class SellController < ApplicationController
-
+  def index
+    @item1 = Item.find(1)
+    @item2 = Item.find(2)
+    @item3 = Item.find(3)
+    @items = Item.all
+  end
   def new
     @item = Item.new
   end
