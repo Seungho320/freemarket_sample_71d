5f02ab00bbc689097b93718355f66547017b8d0e
Seungho320 <splovetw@gmail.com>
2020-03-25 15:15:18 +0900

    make category 1→2

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -2,11 +2,11 @@ class ToppageController < ApplicationController
 
   def index
     @items = Item.limit(10).order('created_at DESC')
-    @category = Categories.all
+    @category = Category.all
 
-    @main_category = Categories.where(sub: '0')
-    @categories = Categories.where(sub: params[:sub], sub_sub: params[:sub_sub])
-    # @sub_sub_category = Category.where(sub: params[:sub], sub_sub: params[:sub_sub] )
+    @main_category = Category.where(sub: '0')
+    @categories = Category.where(sub: params[:id], sub_sub: params[:sub_sub])
+    @sub_sub_categories = Category.where(sub: params[:sub_sub], sub_sub: params[:sub] )
     respond_to do |format|
       format.html
       format.json
