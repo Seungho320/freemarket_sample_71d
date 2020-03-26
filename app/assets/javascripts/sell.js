503299e3168c94efabb5b1c2214bbc15841f4088
jun1t <junichi.hobby@gmail.com>
2020-03-26 18:58:14 +0900

    画像数制限

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -12,6 +12,17 @@ $(document).on('turbolinks:load', ()=> {
     const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
+  const buildFileField2 = (index)=> {
+    const html = `<div class="js-input">
+                    <div class="image-file_group" data-index="${index}">
+                      <input class="image-file" type="file" 
+                      name="item[item_imgs_attributes][${index}][img]"
+                      id="item_item_imgs_attributes_${index}_img">
+                      <div class="js-remove">削除</div>
+                    </div>
+                  </div>`;
+    return html;
+  }
 
   let fileIndex = [1,2,3,4,5];
 
@@ -23,9 +34,12 @@ $(document).on('turbolinks:load', ()=> {
       img.setAttribute('src', blobUrl);
     } else {
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      $('.inputtt').append(buildFileField(fileIndex[0]));
+      $('.js-input').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
+    if($('.image-file_group').length > 5) {
+      $(`.image-file_group[data-index="5"]`).remove();
     }
   });
 
@@ -33,10 +47,9 @@ $(document).on('turbolinks:load', ()=> {
     const targetIndex = $(this).parent().data('index');
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
 
-
     $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
 
-    if ($('.image-file').length == 0) $('.dropbox-container').append(buildFileField(fileIndex[0]));
+    if ($('.image-file').length == 0) $('.dropbox-container').append(buildFileField2(fileIndex[0]));
   });
 });
