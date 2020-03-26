c87607b55ea95ed9aa29d2e0ca7486ff69db75ee
Seungho320 <splovetw@gmail.com>
2020-03-26 19:33:26 +0900

    カテゴリー

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -52,4 +52,78 @@ $(document).on('turbolinks:load', ()=> {
 
     if ($('.image-file').length == 0) $('.dropbox-container').append(buildFileField2(fileIndex[0]));
   });
+
+  // カテゴリーの選択欄追加
+  $(function(){
+    function appendCategory(){
+      let subHTML = `<div class="sell-main__select-wrap">
+                    <select class="sell-main__select-default" name="item[category_id]">
+                      <option value="">--</option>
+                    </select>
+                    </div>`;
+      $(categoryWrapper).append(subHTML);
+    }
+  
+    function appendOption(category, appendWrap){
+      let subOptionHTML =`<option value="${category.id}">${category.name}</option>`;
+      $(appendWrap).children('select').append(subOptionHTML);
+    }
+  
+    const categoryWrapper = '#sell-main__select-category--wrapper';
+    const mainCategory = '#sell-main__select-category--main';
+    const subCategory = '#sell-main__select-category--wrapper div:nth-child(2)';
+    const subSubCategory = '#sell-main__select-category--wrapper div:nth-child(3)';
+  
+    // メインカテゴリー選択時、サブカテゴリー追加
+    $(mainCategory).on('change', function(){
+      let sub = $(this).val();
+      let sub_sub = "0";
+  
+      $.ajax({
+        type: "GET",
+        url: '/sell/new',
+        data: { sub: sub, sub_sub: sub_sub },
+        dataType: 'json'
+      })
+  
+      .done(function(categories){
+        $(subSubCategory).remove();
+        $(subCategory).remove();
+        if(sub != ''){
+          appendCategory();
+          categories.forEach(function(category){
+            appendOption(category, subCategory);
+          });
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー検索に失敗しました');
+      });
+    });
+    
+    // サブカテゴリー選択時、サブサブカテゴリー追加
+    $(document).on('change', subCategory, function(){
+      let sub = $(mainCategory).val();
+      let sub_sub = $(this).children('select').val();
+      $.ajax({
+        type: "GET",
+        url: '/sell/new',
+        data: { sub: sub, sub_sub: sub_sub },
+        dataType: 'json'
+      })
+  
+      .done(function(categories){
+        $(subSubCategory).remove();
+        if(sub_sub != '' && categories[0]){
+          appendCategory();
+          categories.forEach(function(category){
+            appendOption(category, subSubCategory);
+          });
+        }
+      })
+      .fail(function(){
+        alert('カテゴリー検索に失敗しました');
+      });
+    });
+  });
 });
