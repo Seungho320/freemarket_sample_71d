081bb6aae959f258ffd0a11e7143983977c09713
shun0301 <xxsu007xx@icloud.com>
2020-03-27 17:44:29 +0900

    購入機能ー未完成

---
+++ app/controllers/purchase_controller.rb
@@ -0,0 +1,31 @@
+class PurchaseController < ApplicationController
+  
+  require 'payjp'
+
+  def index
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
+  def pay
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :customer => card.customer_id, #顧客ID
+    :currency => 'jpy', #日本円
+  )
+  redirect_to action: 'done' #完了画面に移動
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'purchase/index'
+
+  get 'purchase/done'
+
   get 'card/new'
 
   get 'card/show'
@@ -24,5 +28,13 @@ Rails.application.routes.draw do
         post 'delete', to: 'card#delete'
       end
     end
+  
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
+  end
 end
   
