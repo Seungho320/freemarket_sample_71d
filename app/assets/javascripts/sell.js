0dab87b64ff82e2a982f380e92d60f2cf8721871
jun1t <junichi.hobby@gmail.com>
2020-03-29 00:16:57 +0900

    fix sell new,edit

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', ()=> {
   const buildFileField = (index)=> {
-    const html = `<div class="image-file_group" data-index="${index}">
-                    <input class="image-file" type="file" 
-                    name="item[item_imgs_attributes][${index}][img]"
-                    id="item_item_imgs_attributes_${index}_img">
+    const html = `<div class="js-file_group" data-index="${index}">
+                    <input class="js-file" type="file" 
+                    name="item[item_imgs_attributes][${index}][src]"
+                    id="item_item_imgs_attributes_${index}_src">
                     <div class="js-remove">削除</div>
                   </div>`;
     return html;
@@ -12,21 +12,9 @@ $(document).on('turbolinks:load', ()=> {
     const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
-  const buildFileField2 = (index)=> {
-    const html = `<div class="js-input">
-                    <div class="image-file_group" data-index="${index}">
-                      <input class="image-file" type="file" 
-                      name="item[item_imgs_attributes][${index}][img]"
-                      id="item_item_imgs_attributes_${index}_img">
-                      <div class="js-remove">削除</div>
-                    </div>
-                  </div>`;
-    return html;
-  }
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
-  let fileIndex = [1,2,3,4,5];
-
-  $('.dropbox-container').on('change', '.image-file', function(e) {
+  $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
@@ -34,23 +22,21 @@ $(document).on('turbolinks:load', ()=> {
       img.setAttribute('src', blobUrl);
     } else {
       $('#previews').append(buildImg(targetIndex, blobUrl));
-      $('.js-input').append(buildFileField(fileIndex[0]));
+      $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
     }
-    if($('.image-file_group').length > 5) {
-      $(`.image-file_group[data-index="5"]`).remove();
+    if ($('.js-file').length == 5) {
+      $(`.js-file_group[data-index="5"]`).remove();
     }
   });
 
-  $('.dropbox-container').on('click', '.js-remove', function() {
+  $('#image-box').on('click', '.js-remove', function() {
     const targetIndex = $(this).parent().data('index');
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-
     $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
-
-    if ($('.image-file').length == 0) $('.dropbox-container').append(buildFileField2(fileIndex[0]));
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
 
   // カテゴリーの選択欄追加
