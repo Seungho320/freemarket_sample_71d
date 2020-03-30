11cce1dd841b7f052315bc9a9b4976938a9bfd97
ryo6439 <61182531+ryo6439@users.noreply.github.com>
2020-03-30 14:18:27 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,4 @@
-class Image < CarrierWave::::Base
+class ItemImgsUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
