f6f147ed37475c0e32c5c1a5f1e07b78f87e29ad
jun1t <junichi.hobby@gmail.com>
2020-03-30 16:07:18 +0900

    fix

--- app/controllers/sell_controller.rb
+++ app/controllers/sell_controller.rb
@@ -40,7 +40,7 @@ class SellController < ApplicationController
   end
 
   def update
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
